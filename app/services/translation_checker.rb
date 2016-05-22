529963201475c1c378d194c1db30df5e5878168f
Alex Kochurov <alex.kochurov@gmail.com>
2016-05-22 09:56:41 +0300

    paying technical debt

--- app/services/translation_checker.rb
+++ app/services/translation_checker.rb
@@ -1,34 +1,29 @@
 class TranslationChecker
-  class CheckResult
-    attr_reader :distance
-
-    def initialize(distance)
-      @distance = distance
-    end
-
+  CheckResult = Struct.new(:distance) do
     def success?
       distance <= 1
     end
   end
 
+  attr_reader :card
+
+  delegate :translated_text, :interval, :repeat,
+           :efactor, :attempt, :distance, to: :card
 
   def initialize(card)
     @card = card
   end
 
   def check(user_translation)
-    distance = Levenshtein.distance(full_downcase(@card.translated_text),
+    distance = Levenshtein.distance(full_downcase(translated_text),
                                     full_downcase(user_translation))
 
-    # Не смог найти и вспомнить, как заставить код выполняться, как если бы
-    # self-ом был бы объект @card
-    sm_hash = SuperMemo.algorithm(@card.interval, @card.repeat, @card.efactor,
-                                  @card.attempt, distance, 1)
+    sm_hash = SuperMemo.algorithm(interval, repeat, efactor, attempt, distance, 1)
 
     if distance <= 1
-      sm_hash.merge!(review_date: Time.now + @card.interval.to_i.days, attempt: 1)
+      sm_hash.merge!(review_date: Time.now + interval.to_i.days, attempt: 1)
     else
-      sm_hash.merge!(attempt: [5, @card.attempt + 1].min)
+      sm_hash.merge!(attempt: [5, attempt + 1].min)
     end
 
     @card.update!(sm_hash)
