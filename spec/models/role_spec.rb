315ce32586fe4a8622007cbd23e65e5c58583ee8
Alex Kochurov <alex.kochurov@gmail.com>
2016-06-01 15:57:21 +0300

    fix tests

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates the role object when add new role to user" do
+    initial_amount = Role.count
+    user = User.create(email: 't@t.com', password: '12345', password_confirmation: '12345')
+    user.add_role :absolutely_new_role
+
+    expect(Role.count).to equal(initial_amount + 1)
+  end
 end
