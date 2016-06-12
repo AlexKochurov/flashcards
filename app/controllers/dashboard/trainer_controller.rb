09d680a7e3c2400c52a37834e3d85fe4e0326d0f
Alex Kochurov <alex.kochurov@gmail.com>
2016-06-12 16:02:43 +0300

    Fix: the form in trainer should not be remote (ajax)

--- app/controllers/dashboard/trainer_controller.rb
+++ app/controllers/dashboard/trainer_controller.rb
@@ -19,10 +19,11 @@ class Dashboard::TrainerController < Dashboard::BaseController
       if check_result.distance == 0
         flash[:notice] = t(:correct_translation_notice)
       else
-        flash[:alert] = t 'translation_from_misprint_alert',
-                          user_translation: trainer_params[:user_translation],
-                          original_text: @card.original_text,
-                          translated_text: @card.translated_text
+        flash[:alert] =
+          t('translation_from_misprint_alert',
+            user_translation: trainer_params[:user_translation],
+            original_text:    @card.original_text,
+            translated_text:  @card.translated_text)
       end
       redirect_to trainer_path
     else
